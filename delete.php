426075c1c55400ffac7298261b7d90a92b91e000
Unknown <simonklimke@web.de>
2016-10-16 00:01:42 +0200

    reverted to urldecode

--- delete.php
+++ delete.php
@@ -1,10 +1,10 @@
 <?php
 set_time_limit(120);
 foreach($_POST as $path){
-	if(unlink(realpath(urldecode($path)))){
+	if(unlink(urldecode($path))){
 		echo "Deleted: " . urldecode($path) . "<br>";
 	} else {
-		echo "Failed Deleting: " . urldecode($path) . ", " . realpath($path) . ", " . $path . "<br>";
+		echo "Failed Deleting: " . urldecode($path) . "<br>";
 	}
 }
 ?>
