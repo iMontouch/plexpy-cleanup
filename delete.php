f9de1856191a8b99336075b9cb58b4b885bf55ad
Unknown <simonklimke@web.de>
2016-10-15 23:39:27 +0200

    added more descriptive output

--- delete.php
+++ delete.php
@@ -1,6 +1,9 @@
 <?php
 foreach($_POST as $path){
-	echo "Deleted: " . urldecode($path) . "<br>";
-	unlink(urldecode($path));
+	if(unlink(urldecode($path))){
+		echo "Deleted: " . urldecode($path) . "<br>";
+	} else {
+		echo "Failed Deleting: " . urldecode($path) . "<br>";
+	}
 }
 ?>
